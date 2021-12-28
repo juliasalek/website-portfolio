e99707daae35d734a913bc30dcb8b9b67f58304d
Julia Sałek <julia.salek@comarch.com>
2021-12-28 19:02:05 +0100

    Add header and description to portofolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'websitePortfolio';
+  title = 'Julia Sałek Portfolio';
+
+  public downloadCV(): void {
+
+  }
 }
